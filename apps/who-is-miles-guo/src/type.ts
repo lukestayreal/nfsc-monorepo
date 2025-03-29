13b331f793c98dd5512b0350154efdf5d8127de7
Gu Zeng <guzeng0516@gmail.com>
2025-03-28 19:36:03 -0700

    add who-is-miles-guo

---
+++ apps/who-is-miles-guo/src/type.ts
@@ -0,0 +1,52 @@
+import {
+  Author,
+  Category,
+  SanityImageAsset,
+  SanityImageCrop,
+  SanityImageHotspot,
+  Slug,
+} from './sanity/types';
+
+export type Post = {
+  _id: string;
+  _type: 'post';
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  title?: string;
+  slug?: Slug;
+  author?: Author;
+  mainImage?: SanityImageAsset;
+  categories?: Category[];
+  publishedAt?: string;
+  body?: Array<
+    | {
+        children?: Array<{
+          marks?: Array<string>;
+          text?: string;
+          _type: 'span';
+          _key: string;
+        }>;
+        style?: 'normal' | 'h1' | 'h2' | 'h3' | 'h4' | 'blockquote';
+        listItem?: 'bullet';
+        markDefs?: Array<{
+          href?: string;
+          _type: 'link';
+          _key: string;
+        }>;
+        level?: number;
+        _type: 'block';
+        _key: string;
+      }
+    | {
+        asset?: SanityImageAsset;
+        hotspot?: SanityImageHotspot;
+        crop?: SanityImageCrop;
+        alt?: string;
+        _type: 'image';
+        _key: string;
+      }
+  >;
+  isFeatured?: boolean;
+  excerpt?: string;
+};
