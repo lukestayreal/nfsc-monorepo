cd3bc1b0c3e3cc4fdd0ef91702ea5f439b2ff10f
Gu Zeng <guzeng0516@gmail.com>
2025-04-01 01:27:20 -0700

    better local names

--- apps/who-is-miles-guo/src/i18n/routing.ts
+++ apps/who-is-miles-guo/src/i18n/routing.ts
@@ -1,9 +1,6 @@
 import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
-  locales: ['en', 'zh-hans', 'zh-hant'],
-
-  // Used when no locale matches
+  locales: ['en', 'zh-Hans', 'zh-Hant'],
   defaultLocale: 'en',
 });
