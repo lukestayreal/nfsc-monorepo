2cb314104b30d0be0a6c518a97fed2bcfabb2933
Gu Zeng <guzeng0516@gmail.com>
2025-04-01 01:21:58 -0700

    update i18n locale names

--- apps/who-is-miles-guo/src/i18n/routing.ts
+++ apps/who-is-miles-guo/src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'zh-CN', 'zh-TW'],
+  locales: ['en', 'zh-hans', 'zh-hant'],
 
   // Used when no locale matches
   defaultLocale: 'en',
