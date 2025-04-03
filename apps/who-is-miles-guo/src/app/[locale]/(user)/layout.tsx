359fb21318dae1d53fff9f82238910850c69e7a0
Gu Zeng <guzeng0516@gmail.com>
2025-04-02 17:08:55 -0700

    cleanup metadata

--- apps/who-is-miles-guo/src/app/[locale]/(user)/layout.tsx
+++ apps/who-is-miles-guo/src/app/[locale]/(user)/layout.tsx
@@ -1,16 +1,9 @@
-import type { Metadata } from 'next';
-
 import Navbar from '@/components/navbar';
 import Container from '@/components/container';
 import { GradientBackground } from '@/components/gradient';
 
 import Footer from '@/components/footer';
 
-export const metadata: Metadata = {
-  title: 'My - Blog',
-  description:
-    'Stay informed with product updates, company news, and insights on how to sell smarter at your company.',
-};
 export default async function UserLayout({
   children,
 }: Readonly<{
