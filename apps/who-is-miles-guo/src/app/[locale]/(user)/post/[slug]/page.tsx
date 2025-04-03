b42e12b4064a6055affcad4adf290aa93bd20cc2
Gu Zeng <guzeng0516@gmail.com>
2025-04-02 23:31:12 -0700

    fix build

--- apps/who-is-miles-guo/src/app/[locale]/(user)/post/[slug]/page.tsx
+++ apps/who-is-miles-guo/src/app/[locale]/(user)/post/[slug]/page.tsx
@@ -11,7 +11,7 @@ import { notFound } from 'next/navigation';
 import React from 'react';
 import type { Metadata } from 'next';
 import Markdown from '@/components/markdown';
-import { getLocale, getTranslations, setRequestLocale } from 'next-intl/server';
+import { getTranslations, setRequestLocale } from 'next-intl/server';
 import { LocaleEnum } from '../../../../../../constants/app.constants';
 import { Link } from '@/i18n/navigation';
 import { FeatureFlags } from '@/constant';
