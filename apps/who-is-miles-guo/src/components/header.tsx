510cbfc10062509c8b56f9f6b015fa591bd7aeff
Gu Zeng <guzeng0516@gmail.com>
2025-04-04 18:08:49 -0700

    fix build

--- apps/who-is-miles-guo/src/components/header.tsx
+++ apps/who-is-miles-guo/src/components/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { use, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
 import {
